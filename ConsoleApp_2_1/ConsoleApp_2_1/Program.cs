06b4c305faea22438ad5d461c2bbd6fed7a0a70c
Oxygen-182 <119591922+Oxygen-182@users.noreply.github.com>
2022-12-02 18:02:37 +0300

    Add files via upload

---
+++ ConsoleApp_2_1/ConsoleApp_2_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0, y = 0;
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите координату x: ");
+                    x = double.Parse(Console.ReadLine());
+
+                    Console.Write("Введите координату y: ");
+                    y = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}\n\n");
+                }
+            }
+
+            if (x * x + y * y < 100 && y < Math.Abs(x))
+                Console.WriteLine("\n\nТочка находится внутри окружности");
+            else if (x * x + y * y > 100 || y > Math.Abs(x))
+                Console.WriteLine("\n\nТочка находится вне окружности");
+            else
+                Console.WriteLine("\n\nТочка находится на границе окружности");
+        }
+    }
+}
---
+++ ConsoleApp_2_1_form/ConsoleApp_2_1_form/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp_2_1_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            double x = double.Parse(textBox1.Text);
+            double y = double.Parse(textBox2.Text);
+
+            if (x * x + y * y < 100 && y < Math.Abs(x))
+                textBox3.Text += "Точка находится внутри окружности";
+            else if (x * x + y * y > 100 || y > Math.Abs(x))
+                textBox3.Text += "Точка находится вне окружности";
+            else
+                textBox3.Text += "Точка находится на границе окружности";
+        }
+    }
+}
