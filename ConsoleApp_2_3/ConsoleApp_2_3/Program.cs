54d5c8f0457e4e746049e16b13aeab84d17cbe07
Oxygen-182 <119591922+Oxygen-182@users.noreply.github.com>
2022-12-02 18:04:51 +0300

    Add files via upload

---
+++ ConsoleApp_2_3/ConsoleApp_2_3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вывод всех трехзначных числел, которые начинаются и заканчиваются на одну и ту же цифру\n");
+            for (int n = 101; n <= 999; n ++)
+            {
+                if (n % 10 == n / 100)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+
+            Console.WriteLine("\nВывод всех трехзначных числел, которые начинаются и заканчиваются на одну и ту же цифру\n");
+            int i = 101;
+            while (i < 1000)
+            {
+                if (i % 10 == i / 100)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+
+            Console.WriteLine("\nВывод всех трехзначных числел, которые начинаются и заканчиваются на одну и ту же цифру\n");
+            i = 101;
+            do
+            {
+                if (i % 10 == i / 100)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            } 
+            while (i < 1000);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ConsoleApp_2_3_form/ConsoleApp_2_3_form/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp_2_3_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+
+            if (radioButton1.Checked)
+            {
+                for (int n = 101; n <= 999; n++)
+                {
+                    if (n % 10 == n / 100)
+                    {
+                        textBox1.Text += n + Environment.NewLine;
+                    }
+                }
+            }
+
+            if (radioButton2.Checked)
+            {
+                int i = 101;
+                while (i < 1000)
+                {
+                    if (i % 10 == i / 100)
+                    {
+                        textBox1.Text += i + Environment.NewLine;
+                    }
+                    i++;
+                }
+            }
+
+            if (radioButton3.Checked)
+            {
+                int i = 101;
+                do
+                {
+                    if (i % 10 == i / 100)
+                    {
+                        textBox1.Text += i + Environment.NewLine;
+                    }
+                    i++;
+                }
+                while (i < 1000);
+            }
+        }
+    }
+}
