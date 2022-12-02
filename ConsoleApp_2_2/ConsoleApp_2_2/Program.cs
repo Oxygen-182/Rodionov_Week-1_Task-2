d7b9772e82aeeb38e759cc2bc4adde15fe4d9750
Oxygen-182 <119591922+Oxygen-182@users.noreply.github.com>
2022-12-02 18:03:53 +0300

    Add files via upload

---
+++ ConsoleApp_2_2/ConsoleApp_2_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_2_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите номер телевизионного канала (1-10): ");
+                    n = int.Parse(Console.ReadLine());
+
+                    if (n < 0) throw new Exception("Номер телевизионного канала не может быть отрицательным");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}\n\n");
+                }
+            }
+
+            Console.Write("\nНАИБОЛЕЕ ПОПУЛЯРНЫЕ ПРОГРАММЫ:\n");
+
+            switch (n)
+            {
+                case 1: Console.WriteLine("\nВести\nЧто? Где? Когда?\nВремя покажет\n"); break;
+                case 2: Console.WriteLine("\n60 минут\nКто против?\nВечер с Владимиром Соловьевым\n"); break;
+                case 3: Console.WriteLine("\nЧрезвычайное происшествие\nСегодня\nМесто встречи\n"); break;
+                case 4: Console.WriteLine("\nНовости культуры\nИскусственный отбор\nЗабытое ремесло\n"); break;
+                case 5: Console.WriteLine("\nИзвестия\nСлед\nДетективы\n"); break;
+                case 6: Console.WriteLine("\nСпокойной ночи, малыши!\nЧто в тарелке?\nИгра с умом\n"); break;
+                case 7: Console.WriteLine("\nЗа Дело!\nКалендарь\nНовости\n"); break;
+                case 8: Console.WriteLine("\nТерритория заблуждений\nЗасекреченные списки\nТайны Чапман\n"); break;
+                case 9: Console.WriteLine("\nСпециальный репортаж\nОткрытый эфир\nМежду тем\n"); break;
+                case 10: Console.WriteLine("\nВечер во Владимире\nВечерний зефир\nТелефорум\n"); break;
+                default: Console.WriteLine("ВЫ ОШИБЛИСЬ"); break;
+            }
+        }
+    }
+}
---
+++ ConsoleApp_2_2_form/ConsoleApp_2_2_form/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ConsoleApp_2_2_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            int n = int.Parse(textBox1.Text);
+
+            if (n % 1 == 0) { }
+            else
+            {
+                textBox2.Text += "Номер телевизионного канала введено некорректно";
+                return;
+            }
+
+            if (n < 0)
+            {
+                textBox2.Text += "Номер телевизионного канала не может быть отрицательным";
+                return;
+            }
+            switch (n)
+            {
+                case 1:
+                    textBox2.Text += "НАИБОЛЕЕ ПОПУЛЯРНЫЕ ПРОГРАММЫ:" + Environment.NewLine;
+                    textBox2.Text += Environment.NewLine + "Вести" + Environment.NewLine + "Что? Где? Когда?" + Environment.NewLine + "Время покажет"; break;
+                case 2:
+                    textBox2.Text += "НАИБОЛЕЕ ПОПУЛЯРНЫЕ ПРОГРАММЫ:" + Environment.NewLine;
+                    textBox2.Text += Environment.NewLine + "60 минут" + Environment.NewLine + "Кто против?" + Environment.NewLine + "Вечер с Владимиром Соловьевым"; break;
+                case 3:
+                    textBox2.Text += "НАИБОЛЕЕ ПОПУЛЯРНЫЕ ПРОГРАММЫ:" + Environment.NewLine;
+                    textBox2.Text += Environment.NewLine + "Чрезвычайное происшествие" + Environment.NewLine + "Сегодня" + Environment.NewLine + "Место встречи"; break;
+                case 4:
+                    textBox2.Text += "НАИБОЛЕЕ ПОПУЛЯРНЫЕ ПРОГРАММЫ:" + Environment.NewLine;
+                    textBox2.Text += Environment.NewLine + "Новости культуры" + Environment.NewLine + "Искусственный отбор" + Environment.NewLine + "Забытое ремесло"; break;
+                case 5:
+                    textBox2.Text += "НАИБОЛЕЕ ПОПУЛЯРНЫЕ ПРОГРАММЫ:" + Environment.NewLine;
+                    textBox2.Text += Environment.NewLine + "Известия" + Environment.NewLine + "След" + Environment.NewLine + "Детективы"; break;
+                case 6:
+                    textBox2.Text += "НАИБОЛЕЕ ПОПУЛЯРНЫЕ ПРОГРАММЫ:" + Environment.NewLine;
+                    textBox2.Text += Environment.NewLine + "Спокойной ночи, малыши!" + Environment.NewLine + "Что в тарелке?" + Environment.NewLine + "Игра с умом"; break;
+                case 7:
+                    textBox2.Text += "НАИБОЛЕЕ ПОПУЛЯРНЫЕ ПРОГРАММЫ:" + Environment.NewLine;
+                    textBox2.Text += Environment.NewLine + "За Дело!" + Environment.NewLine + "Календарь" + Environment.NewLine + "Новости"; break;
+                case 8:
+                    textBox2.Text += "НАИБОЛЕЕ ПОПУЛЯРНЫЕ ПРОГРАММЫ:" + Environment.NewLine;
+                    textBox2.Text += Environment.NewLine + "Территория заблуждений" + Environment.NewLine + "Засекреченные списки" + Environment.NewLine + "Тайны Чапман"; break;
+                case 9:
+                    textBox2.Text += "НАИБОЛЕЕ ПОПУЛЯРНЫЕ ПРОГРАММЫ:" + Environment.NewLine;
+                    textBox2.Text += Environment.NewLine + "Специальный репортаж" + Environment.NewLine + "Открытый эфир" + Environment.NewLine + "Между тем"; break;
+                case 10:
+                    textBox2.Text += "НАИБОЛЕЕ ПОПУЛЯРНЫЕ ПРОГРАММЫ:" + Environment.NewLine;
+                    textBox2.Text += Environment.NewLine + "Вечер во Владимире" + Environment.NewLine + "Вечерний зефир" + Environment.NewLine + "Телефорум"; break;
+                default: textBox2.Text += "ВЫ ОШИБЛИСЬ"; break;
+            };
+        }
+    }
+}
